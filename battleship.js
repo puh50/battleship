097a48c85fa2f3857265b3916468bba314c662cc
puh50 <puhpuh690@gmail.com>
2019-02-04 22:50:38 +0300

    model object

--- battleship.js
+++ battleship.js
@@ -1,34 +1,52 @@
-var randomLoc = Math.floor(Math.random() * 5);
-var location1 = randomLoc;
-var location2 = location1 + 1;
-var location3 = location2 + 1;
-
-var guess;
-var hits = 0;
-var guesses = 0;
-
-var isSunk = false;
-
-while (isSunk==false) {
-	guess = prompt("Ready, aim, fire! (enter a number 0-6):"); 
-	if (guess < 0 || guess > 6) {
-		alert("Please enter a valid cell number!")
-	} else {
-		guesses += 1;
+var view = {
+	displayMessage: function(msg) {
+		var messageArea = document.getElementById("messageArea");
+		messageArea.innerHTML = msg;
+	},
+	displayHit: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "hit");
+	},
+	displayMiss: function(location) {
+		var cell = document.getElementById(location);
+		cell.setAttribute("class", "miss");
 	}
+};
 
-	if (guess == location1  || guess == location2 || guess == location3) {
-		alert("HIT!");
-		hits += 1;
-
-		if (hits == 3) {
-			isSunk = true;
-			alert("You sank my battleship!")
-		}
-	} else {
-		alert("MISS");
+var model = {
+	boardSize: 7,
+	numShips: 3,
+	shipsSunk: 0,
+	shipLength: 3,
+	ships: [{ locations: ["06", "16", "26"], hits: ["", "", ""] },
+			{ locations: ["24", "34", "44"], hits: ["", "", ""] },
+			{ locations: ["10", "11", "12"], hits: ["", "", ""] }],
+	fire: function(guess) {
+		for (var i = 0; i < this.numShips; i++) {
+			var ship = this.ships[i];
+			var locations = ship.locations;
+			var index = locations.indexOf(guess);
+			if (index >= 0) {
+				ship.hits[index] = "hit";
+				view.displayHit(guess);
+				view.displayMessage("HIT!");
+				if (this.isSunk(ship)) {
+					view.displayMessage("You sank my battleship!");
+					this.shipsSunk++;
+				};
+			return true;
+			};
+		};
+		view.displayMiss(guess);
+		view.displayMessage("You missed.");
+		return false;
+	},
+	isSunk: function(ship) {
+		for (var i = 0; i < this.shipLength; i++) {
+			if (ship.hits[i] !== "hit") {
+				return false;
+			};
+		};
+	return true;
 	}
-}
-var stats = "You took " + guesses + " guesses to sink the battleship, " +
-"which means your shooting accuracy was " + (3/guesses);
-alert(stats);
+};
